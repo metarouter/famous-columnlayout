2cc9c8b644a7ef6d13238407cb9e12b4ba89a642
Arturas Lebedevas <metarouter@gmail.com>
2014-09-15 20:13:32 +0100

    Cell default width is 1 column; Cell.getSize

--- Cell.js
+++ Cell.js
@@ -7,6 +7,14 @@ function Cell () {
 Cell.prototype = Object.create(View.prototype);
 Cell.prototype.constructor = Cell;
 
-Cell.DEFAULT_OPTIONS = {};
+Cell.DEFAULT_OPTIONS = {
+  // Cell size is measured in number of columns
+  // By default, cell spans across one column
+  size: 1
+};
 
 module.exports = Cell;
+
+Cell.prototype.getSize = function () {
+  return this.options.size;
+};
