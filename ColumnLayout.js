2176265f8a926384870c850d4380305e5eb8fa1a
Arturas Lebedevas <metarouter@gmail.com>
2014-09-30 13:15:25 +0100

    initial sequenceFromArray() implementation

--- ColumnLayout.js
+++ ColumnLayout.js
@@ -2,6 +2,8 @@ var View = require('famous/core/View');
 
 function ColumnLayout () {
   View.apply(this, arguments);
+
+  this._items = [];
 }
 
 ColumnLayout.prototype = Object.create(View.prototype);
@@ -10,3 +12,13 @@ ColumnLayout.prototype.constructor = ColumnLayout;
 ColumnLayout.DEFAULT_OPTIONS = {};
 
 module.exports = ColumnLayout;
+
+ColumnLayout.prototype.sequenceFromArray = function (array) {
+  array.forEach(_addItem.bind(this));
+};
+
+function _addItem (item) {
+  var items = this._items;
+
+  items.push(item);
+}
