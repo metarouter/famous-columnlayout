6d0d5f9cc16a4e4f290c5dca588bbf21951d1d21
Arturas Lebedevas <metarouter@gmail.com>
2014-10-05 11:24:05 +0100

    4 surfaces layout example

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -2,21 +2,46 @@ var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
 
 var ColumnLayout = require('../../ColumnLayout');
-var Cell         = require('../../Cell');
 
 var mainContext = Engine.createContext();
 var surface = new Surface({
   content: 'Hello, world',
+  size: [undefined, true],
   properties: {
     backgroundColor: 'lightblue'
   }
 });
-var cell = new Cell();
-var cells = [cell];
-var layout = new ColumnLayout();
+var surface_2 = new Surface({
+  content: 'Surface 2',
+  size: [undefined, true],
+  properties: {
+    backgroundColor: 'lightgreen',
+    fontSize: '5rem'
+  }
+});
+var surface_3 = new Surface({
+  content: 'Surface 3',
+  size: [undefined, true],
+  properties: {
+    backgroundColor: 'lightyellow',
+    fontSize: '2rem'
+  }
+});
+var surface_4 = new Surface({
+  content: 'Surface 4',
+  size: [undefined, true],
+  properties: {
+    backgroundColor: 'orange',
+    fontSize: '5rem'
+  }
+});
+//surface_3.getCellColumnSize = function () { return 2; };
+var cells = [surface, surface_2, surface_3, surface_4];
+var layout = new ColumnLayout({
+});
+window.layout = layout;
 
-cell.add(surface);
-layout.sequenceFromArray(cells);
+layout.sequenceFrom(cells);
 
 mainContext.add(layout);
 
