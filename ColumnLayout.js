24c5e9f28f567adc603ef9b30826d958f0fa19a0
Arturas Lebedevas <metarouter@gmail.com>
2014-10-05 11:20:38 +0100

    sync reimplementation

--- ColumnLayout.js
+++ ColumnLayout.js
@@ -3,7 +3,8 @@ var OptionsManager = require('famous/core/OptionsManager'),
     Modifier       = require('famous/core/Modifier'),
     Transform      = require('famous/core/Transform'),
     ViewSequence   = require('famous/core/ViewSequence');
-var Transitionable = require('famous/transitions/Transitionable');
+
+var NULL_CELL_SIZE = [0, 0];
 
 function ColumnLayout (options) {
   this.options = Object.create(ColumnLayout.DEFAULT_OPTIONS);
@@ -15,8 +16,8 @@ function ColumnLayout (options) {
   this.sequence = null;
   this.id = Entity.register(this);
   this._modifiers = [];
-  this._positions = [];
-  this._contextSize = new Transitionable([0, 0]);
+  this._cells = [];
+  this._contextSize = [0, 0];
 }
 
 ColumnLayout.DEFAULT_OPTIONS = {
@@ -33,12 +34,12 @@ ColumnLayout.prototype.render = function () {
 
 ColumnLayout.prototype.commit = function (context) {
   var contextSize    = context.size,
-      size           = this._contextSize,
-      sizeCache      = size.get(),
+      sizeCache      = this._contextSize,
       sizeHasChanged = (sizeCache[0] !== contextSize[0]);
 
   if (sizeHasChanged) {
-    size.set([contextSize[0], contextSize[1]], this.options.transition);
+    this._contextSize = [contextSize[0], contextSize[1]];
+    reflow.call(this);
   }
 
   var result = forEachSequenceItem(this.sequence, function (item, index) {
@@ -62,9 +63,67 @@ ColumnLayout.prototype.sequenceFrom = function (sequence) {
     sequence = new ViewSequence(sequence);
   }
   this.sequence = sequence;
+  this._cells = [];
 
   createLayout.call(this);
 };
+
+function reflow () {
+  var cells = this._cells,
+      size  = this._contextSize;
+  var columnInfo = calcColumns(size[0], this.options.columnWidth);
+
+  if (columnInfo.columns === 0) {
+    return;
+  }
+  var columns = [];
+  var columnsHeight = fillArray([], 0, columnInfo.columns);
+
+  for (var i = 0; i < columnInfo.columns; i++) {
+    columns[i] = [];
+  }
+
+  cells.forEach(function (cellInfo, index) {
+    cellInfo.size = cellInfo.size || NULL_CELL_SIZE;
+
+    var columnIndex = minIndex(columnsHeight),
+        column      = columns[columnIndex],
+        cellSize    = cellInfo.size;
+
+    if (column.length > 0) {
+      cellInfo.previous = column[column.length - 1];
+    } else {
+      delete cellInfo.previous;
+    }
+    column.push(index);
+    columnsHeight[columnIndex] += cellSize[1];
+    cellInfo.column = columnIndex;
+  });
+}
+
+function fillArray (array, val, length) {
+  length = length || array.length;
+
+  for (var i = 0; i < length; i++) {
+    array[i] = val;
+  }
+  return array;
+}
+
+function minIndex (array) {
+  var index = -1;
+  var min = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    if (i === 0) { min = array[i]; index = i; continue; }
+
+    if (array[i] < min) {
+      min = array[i];
+      index = i;
+    }
+  }
+  return index;
+}
 
 function createLayout () {
   var sequence = this.sequence;
@@ -83,8 +142,9 @@ function positionItemInLayout (item, index, previousItem) {
                         item.getCellColumnSize() :
                         opts.defaultCellColumnSize);
 
-  modifier.sizeFrom(cellSize.bind(this, item, index, cellColumnSize));
-  modifier.transformFrom(cellPosition.bind(this, index, previousItem));
+  var cellInfo = this._cells[index];
+  modifier.sizeFrom(cellSize.bind(this, item, cellInfo, cellColumnSize));
+  modifier.transformFrom(cellPosition.bind(this, cellInfo));
 }
 
 function calcColumns (contextWidth, columnWidth) {
@@ -96,33 +156,37 @@ function calcColumns (contextWidth, columnWidth) {
   };
 }
 
-function cellPosition (index) {
-  var size    = this._contextSize.get(),
-      columns = calcColumns(size[0], this.options.columnWidth);
-  var xPos = (index % columns.columns) * columns.columnWidth;
-  var yPos = 0;
-  var prevIndex = index - columns.columns;
-  var cellInfo = this._positions[index];
+function cellPosition (cellInfo) {
+  var size       = this._contextSize,
+      columnInfo = calcColumns(size[0], this.options.columnWidth);
+  var previousY      = 0,
+      previousHeight = 0,
+      previous       = this._cells[cellInfo.previous];
 
-  if (prevIndex >= 0) {
-    var previousCellInfo = this._positions[prevIndex];
-    yPos = previousCellInfo.size[1] + previousCellInfo.y;
-  }
+  if (previous && previous.y) previousY = previous.y;
+  if (previous && previous.size) previousHeight = previous.size[1];
+
+  var xPos = (cellInfo.column % columnInfo.columns) * columnInfo.columnWidth;
+  var yPos = previousY + previousHeight;
 
   cellInfo.x = xPos;
   cellInfo.y = yPos;
 
+
   return Transform.translate(xPos, yPos);
 }
 
-function cellSize (item, index, cellColumnSize) {
-  var size    = this._contextSize.get(),
-      trueSize = item.getSize(true) || [0, 0],
-      columns = calcColumns(size[0], this.options.columnWidth);
+function cellSize (item, cellInfo, cellColumnSize) {
+  var size       = this._contextSize,
+      trueSize   = item.getSize(true) || NULL_CELL_SIZE,
+      columnInfo = calcColumns(size[0], this.options.columnWidth);
 
-  var cellWidth = columns.columnWidth * cellColumnSize;
+  var cellWidth = columnInfo.columnWidth * cellColumnSize;
 
-  var cellInfo = this._positions[index];
+  cellInfo.size = cellInfo.size || NULL_CELL_SIZE;
+  if (trueSize[1] !== cellInfo.size[1]) {
+    setTimeout(reflow.bind(this), 1);
+  }
   cellInfo.size = [cellWidth, trueSize[1]];
 
   return [cellWidth, size[1]];
@@ -132,8 +196,11 @@ function createModifier (item, index) {
   if (! this._modifiers[index]) {
     this._modifiers[index] = new Modifier();
   }
-  if (! this._positions[index]) {
-    this._positions[index] = {};
+  if (! this._cells[index]) {
+    this._cells[index] = {
+      index: index,
+      column: index
+    };
   }
 }
 
